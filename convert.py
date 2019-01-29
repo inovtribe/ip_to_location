cde6f4e16ee49d13556f27cc7eab5504a934d82b
konifar <yahpeycoy0403@gmail.com>
2019-01-29 13:02:47 +0900

    Fixed bug

--- convert.py
+++ convert.py
@@ -27,9 +27,9 @@ with open(output_file, mode='w') as f:
     for row in resultMap:
         for i in range(len(row)):
             cell = row[i]
-            f.write(cell['country'])
+            f.write(cell['country'].encode('utf-8'))
             f.write(',')
-            f.write(cell['city'])
+            f.write(cell['city'].encode('utf-8'))
             if (len(row) - 1 > i):
                 f.write(',')
         f.write('\n')
