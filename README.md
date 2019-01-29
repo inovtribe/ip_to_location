2a3f8c5fd3ff6e81eae8542240f0850789ccad9f
konifar <yahpeycoy0403@gmail.com>
2019-01-29 12:24:39 +0900

    Created script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ip_to_location
 Simple script to convert ip to location
+
+## API
+http://ip-api.com/
+
+The limit is 150 requests per minute from an IP address.
+If you go over this limit your IP address will be blackholed. You can unban [here](http://ip-api.com/docs/unban).
+
+## Run
+```shell
+$ pyhon convert.py
+```
---
+++ convert.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import csv
+import time
+
+url = 'http://ip-api.com/json/'
+resultMap = []
+
+# Input
+with open('test_data.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        resultRow = []
+        for cell in row:
+            cell = cell.replace(" ", "")
+            time.sleep(0.5)
+            r = requests.get(url + cell)
+            data = json.loads(r.text)
+            print("country:" + data["country"] + ", city:" + data["city"])
+            map = {'country': data["country"], 'city': data["city"]}
+            resultRow.append(map)
+        resultMap.append(resultRow)
+
+# Output
+output_file = 'output.csv'
+with open(output_file, mode='w') as f:
+    for row in resultMap:
+        for i in range(len(row)):
+            cell = row[i]
+            f.write(cell['country'])
+            f.write(',')
+            f.write(cell['city'])
+            if (len(row) - 1 > i):
+                f.write(',')
+        f.write('\n')
